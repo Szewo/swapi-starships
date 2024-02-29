72ee6aef86b6180c52648f3e6e603f76283b66b5
Szewo <szewczyk.mat@yahoo.com>
2024-02-29 19:40:06 +0100

    Add phpunit config.

--- Makefile
+++ Makefile
@@ -10,9 +10,15 @@ build-with-xdebug:
 	docker compose --env-file .env.debug up --build -d
 debug-mode:
 	XDEBUG_MODE=debug docker compose up -d
+coverage-mode:
+	XDEBUG_MODE=coverage docker compose up -d
 phpstan:
 	docker exec -it swapi-starships-app-1 vendor/bin/phpstan analyse -c phpstan.dist.neon
 cs:
 	docker exec -it swapi-starships-app-1 vendor/bin/php-cs-fixer fix src
 cs-check:
 	docker exec -it swapi-starships-app-1 vendor/bin/php-cs-fixer check src
+unit:
+	docker exec -it swapi-starships-app-1 php bin/phpunit --testdox
+unit-coverage:
+	XDEBUG_MODE=coverage docker exec -it swapi-starships-app-1 php bin/phpunit --testdox --coverage-html ./tmp/coverage
---
+++ app/tests/Response/SwapiResponseTest.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Response;
+
+use App\DTO\StarshipDTO;
+use App\Response\ResponseStatus;
+use App\Response\SwapiResponse;
+use App\Response\SwapiResponseInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(SwapiResponse::class)]
+#[UsesClass(StarshipDTO::class)]
+final class SwapiResponseTest extends TestCase
+{
+    private SwapiResponseInterface $instance;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->instance = new SwapiResponse();
+    }
+
+    public function testCanBeCreatedWithDefaultValues(): void
+    {
+        $this->assertSame(
+            expected: null,
+            actual: $this->instance->getStatus()
+        );
+        $this->assertSame(
+            expected: [],
+            actual: $this->instance->getContent()
+        );
+        $this->assertSame(
+            expected: 'Something went wrong',
+            actual: $this->instance->getError()
+        );
+    }
+
+    public function testCanReturnSuccessStatus(): void
+    {
+        $this->instance->setStatus(ResponseStatus::SUCCESS);
+
+        $this->assertSame(
+            expected: ResponseStatus::SUCCESS,
+            actual: $this->instance->getStatus(),
+        );
+    }
+
+    public function testCanReturnFailureStatus(): void
+    {
+        $this->instance->setStatus(ResponseStatus::FAILURE);
+
+        $this->assertSame(
+            expected: ResponseStatus::FAILURE,
+            actual: $this->instance->getStatus(),
+        );
+    }
+
+    public function testCanReturnError(): void
+    {
+        $this->instance->setError('Test error');
+
+        $this->assertSame(
+            expected: 'Test error',
+            actual: $this->instance->getError(),
+        );
+    }
+
+    public function testCanReturnContent(): void
+    {
+        $this->instance->setContent(
+            content: $this->getArrayWithStarships(),
+        );
+
+        $this->assertIsArray(
+            actual: $this->instance->getContent(),
+        );
+    }
+
+    /**
+     * @return StarshipDTO[]
+     */
+    private function getArrayWithStarships(): array
+    {
+        return [
+            new StarshipDTO(
+                name: 'Test Name',
+                model: 'Model',
+                starshipClass: 'Class',
+                manufacturer: 'Manufacturer',
+                costInCredits: '100000',
+                length: '1000',
+                crew: 'Crew',
+                passengers: 'passengers',
+                maxAtmospheringSpeed: '100000',
+                hyperdriveRating: 2.0,
+                mglt: 'MGLT',
+                cargoCapacity: 'Cargo Capacity',
+                consumables: 'Consumables'
+            )
+        ];
+    }
+}
